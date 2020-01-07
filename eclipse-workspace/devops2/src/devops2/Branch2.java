d314042ad2b552db54a5d29ec8c640bdde44a03c
PC i5 <iaitlmaalem@gmail.com>
2020-01-07 10:27:25 +0100

    Ajout branch2.java

---
+++ eclipse-workspace/devops2/src/devops2/Branch2.java
@@ -0,0 +1,8 @@
+package devops2;
+
+public class Branch2
+{
+	 public static String getResult() {
+		    return "1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, Fizz Buzz";
+	    }
+}
