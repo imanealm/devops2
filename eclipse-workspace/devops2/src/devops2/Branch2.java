1dc1e8dbe888eece272b861508c41663a88c34cf
PC i5 <iaitlmaalem@gmail.com>
2020-01-07 10:52:04 +0100

    commit2

--- eclipse-workspace/devops2/src/devops2/Branch2.java
+++ eclipse-workspace/devops2/src/devops2/Branch2.java
@@ -3,6 +3,7 @@ package devops2;
 public class Branch2
 {
 	 public static String getResult() {
+			System.out.println( "My Branch2" );  
 		    return "1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, Fizz Buzz";
 	    }
 }
